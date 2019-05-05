cc37f087291f512d6f761f493b0c29fb4b803ca9
unknown <marian.brynetskyy@outlook.com>
2019-05-05 21:12:01 +0300

    Remove check for lysyi

--- src/main/java/com/kilopo/KBot.java
+++ src/main/java/com/kilopo/KBot.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import static com.kilopo.Constants.*;
 
 public class KBot extends TelegramLongPollingCommandBot {
-    private String newMessageText;
 
     KBot() {
         super(BOT_NAME);
@@ -29,7 +28,6 @@ public class KBot extends TelegramLongPollingCommandBot {
 
     @Override
     public void processNonCommandUpdate(Update update) {
-        checkLus(update);
         hzShoDelat(update);
     }
 
@@ -53,48 +51,9 @@ public class KBot extends TelegramLongPollingCommandBot {
 
         ) {
 
-            if ((newMessageText = checkName(text)) != null) {
-                newMessageText = newMessageText + " пезда, я не знаю шо дєлать.";
-            } else {
-                newMessageText = text + " пезда, я не знаю шо дєлать.";
-            }
+            String newMessageText = text + " пезда, я не знаю шо дєлать.";
             sendMessage(newMessageText, update);
         }
-    }
-
-    private void checkLus(Update update) {
-        newMessageText = checkName(update.getMessage().getText());
-        if (newMessageText != null) {
-            newMessageText = "Ви мали на увазі " + newMessageText + " ?";
-            sendMessage(newMessageText, update);
-        }
-    }
-
-    private String checkName(String message) {
-        message = message.toLowerCase();
-        String[] words = message.split(" ");
-
-        if (Arrays.stream(words).anyMatch(word -> word.contains("лисий") || word.contains("лиcий"))) {
-            return "Мар'ян";
-        } else if (Arrays.stream(words).anyMatch(word -> word.contains("лисого")
-                || word.contains("лиcого") || word.contains("лисoго") || word.contains("лисоrо") || word.contains("лисогo")
-                || word.contains("лиcoго") || word.contains("лиcоrо") || word.contains("лиcогo")
-                || word.contains("лисorо") || word.contains("лисoгo")
-                || word.contains("лисоro")
-                || word.contains("лиcorо") || word.contains("лиcoгo") || word.contains("лиcоro")
-                || word.contains("лиcoro"))) {
-            return "Мар'яна";
-        } else if (Arrays.stream(words).anyMatch(word -> word.contains("лисому") || word.contains("лиcому") || word.contains("лисoму") || word.contains("лисоmу") || word.contains("лисомy")
-                || word.contains("лиcoму") || word.contains("лиcоmу") || word.contains("лиcомy")
-                || word.contains("лисomу") || word.contains("лисoмy")
-                || word.contains("лисоmy")
-                || word.contains("лиcomу") || word.contains("лиcoмy") || word.contains("лиcоmy")
-                || word.contains("лисomy"))) {
-            return "Мар'яну";
-        } else if (Arrays.stream(words).anyMatch(word -> word.contains("лисим") || word.contains("лиcим") || word.contains("лисиm") || word.contains("лиcиm"))) {
-            return "Мар'яном";
-        } else
-            return null;
     }
 
     private void sendMessage(String text, Update update) {
